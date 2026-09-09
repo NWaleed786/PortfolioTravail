bfb6584e469b40be8d1131ac833ed1f423847fc5
Nasrulla Waleed <WaleedMega786@gmail.com>
2026-09-09 19:20:35 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -2,7 +2,7 @@
 #
 # Pour un dépôt GitHub Pages de type projet, les liens du site restent relatifs.
 # Si vous souhaitez définir une URL complète, vous pouvez lancer :
-#   SITEURL="https://votre-compte.github.io/votre-depot" pelican content -s publishconf.py
+#   SITEURL="https://github.com/NWaleed786/PortfolioTravail.git" pelican content -s publishconf.py
 
 import os
 import sys
@@ -10,6 +10,6 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = os.environ.get("SITEURL", "")
+SITEURL = os.environ.get("SITEURL", "https://github.com/NWaleed786/PortfolioTravail.git")
 RELATIVE_URLS = True
 DELETE_OUTPUT_DIRECTORY = True
